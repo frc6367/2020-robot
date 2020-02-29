78f17a566df8bf608dcdb6baf2ed00e07e06ec93
tacoqueenme <46407033+tacoqueenme@users.noreply.github.com>
2020-02-29 17:34:58 -0500

    Update robot.py

--- simvalue/robot.py
+++ simvalue/robot.py
@@ -28,6 +28,10 @@ class MyRobot(wpilib.TimedRobot):
 
         self.balls = [self.ball_device.createDouble(str(i), False, float('nan')) for i in range(5)]
 
+        self.present = {}
+
+        self.notPresent = {i: True for i in range(5)}
+
         self.sensor1 = wpilib.AnalogInput(1)
         self.sensorSim1 = hal.simulation.AnalogInSim(1)
 
@@ -56,11 +60,23 @@ class MyRobot(wpilib.TimedRobot):
         # is ball inserted?
         if self.ball_insert.get():
             # 'insert' a new ball
-            print("insert detected")
-            self.balls[0].set(0)
+            if self.notPresent:
+                for i in self.notPresent.keys():
+                    break
+
+                self.notPresent.pop(i)
+                self.present[i] = True
+
+                ball = self.balls[i] # The ball itself
+                ball.set(0) # Position
+            
             self.ball_insert.set(False)
 
         # sim code goes here
+
+        print("---")
+        print(self.present)
+        print(self.notPresent)
 
         # position: velocity*tm_diff
         belt_velocity = self.belt_motor_sim.getSpeed()
@@ -71,6 +87,16 @@ class MyRobot(wpilib.TimedRobot):
         # otherwise set to 0
 
         # compute if balls overlap with each other when overlapping the belt motor. If so, raise an exception
+        # for balls in range(1,5):
+        #     if  balls 
+        if self.balls[2] < 0:
+            
+            pass
+        elif 0 < self.balls[2] < 14.5:
+            self.balls[2].position += .5 * tm_diff
+        elif 15 < self.balls[2] < 43:
+            self.balls[2].position += belt_velocity
+
 
         self.sensorSim1.setVoltage(0)
 
