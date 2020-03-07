24a7af0599d038e155151c67eb05f907092ee7a4
JonnyG511 <35736713+JonnyG511@users.noreply.github.com>
2020-03-07 16:07:19 -0500

    added
    We added code that stops the robot from intaking as soon as it is enabled.

--- sim_test/robot.py
+++ sim_test/robot.py
@@ -19,7 +19,7 @@ class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         """Robot-wide initialization code should go here"""
         self.joystick = wpilib.Joystick(0)
-        self.spark = rev.CANSparkMax(5,rev.CANSparkMax.MotorType.kBrushless)
+        self.spark = rev.CANSparkMax(5, rev.CANSparkMax.MotorType.kBrushless)
         self.talon = ctre.TalonSRX(6)
         self.speed = 0
 
@@ -34,28 +34,25 @@ class MyRobot(wpilib.TimedRobot):
         # Position gets automatically updated as robot moves
         # self.gyro = wpilib.AnalogGyro(1)
 
-    
-
         self.drive = wpilib.drive.DifferentialDrive(self.l_motor, self.r_motor)
-        self.drive2 = wpilib.drive.DifferentialDrive(self.l_motor2,self.r_motor2)
-
+        self.drive2 = wpilib.drive.DifferentialDrive(self.l_motor2, self.r_motor2)
 
         self.sensor1 = wpilib.AnalogInput(0)
         self.sensor2 = wpilib.AnalogInput(1)
         self.sensor3 = wpilib.AnalogInput(2)
 
-        # state (steps/stages) for the intake 
+        # state (steps/stages) for the intake
         # refer to def operatedIntake(self) about implementation
         self.step = -1
 
-        # value for the sensor that detects if ball is there  
-        # if the value is above 1.8 the ball is there otherwise it is not 
+        # value for the sensor that detects if ball is there
+        # if the value is above 1.8 the ball is there otherwise it is not
         self.kBallTreshhold = 1.6
 
-        self.kBeltSpeed = -.3
-       
-       # if in simulation mode it turns on the physics engine
-       # initializes time  
+        self.kBeltSpeed = -0.3
+
+        # if in simulation mode it turns on the physics engine
+        # initializes time
 
         if is_sim:
             self.physics = physics.PhysicsEngine()
@@ -76,37 +73,46 @@ class MyRobot(wpilib.TimedRobot):
         self.timer.start()
 
     def autonomousPeriodic(self):
-        """Called every couple of milseconds""" 
+        """Called every couple of milseconds"""
         if self.timer.get() < 2.0:
             self.drive.arcadeDrive(-1.0, -0.3)
         else:
             self.drive.arcadeDrive(0, 0)
 
+    def teleopInit(self):
+        self.step = -1
+        self.spark.set(0)
+        self.talon.set(ctre.ControlMode.PercentOutput, 0)
+
     def teleopPeriodic(self):
         """Called when operation control mode is enabled"""
-        
+
         # self.drive.arcadeDrive(self.lstick.getY(), self.lstick.getX())
-        #self.drive.arcadeDrive(self.lstick.getRawAxis(1), self.lstick.getRawAxis(3))
+        # self.drive.arcadeDrive(self.lstick.getRawAxis(1), self.lstick.getRawAxis(3))
 
         # Move a motor with a Joystick
 
-        self.drive.arcadeDrive(-self.joystick.getY(),self.joystick.getX(),False)
-        self.drive2.arcadeDrive(-self.joystick.getY(),self.joystick.getX(),False)
+        self.drive.arcadeDrive(
+            -self.joystick.getY(), self.joystick.getRawAxis(2), False
+        )
+        self.drive2.arcadeDrive(
+            -self.joystick.getY(), self.joystick.getRawAxis(2), False
+        )
 
         self.autoIntake()
 
     def operatedIntake(self):
         if self.joystick.getRawButton(1):
             self.speed = self.joystick.getRawAxis(3)
-            self.speed = 1 - (self.speed + 1)/2
-            self.talon.set(ctre.ControlMode.PercentOutput,-self.speed)
+            self.speed = 1 - (self.speed + 1) / 2
+            self.talon.set(ctre.ControlMode.PercentOutput, -self.speed)
         elif self.joystick.getRawButton(2):
             self.speed = self.joystick.getRawAxis(3)
-            self.speed = 1 - (self.speed + 1)/2
+            self.speed = 1 - (self.speed + 1) / 2
             self.spark.set(-self.speed)
         else:
             self.spark.set(0)
-            self.talon.set(ctre.ControlMode.PercentOutput,0)
+            self.talon.set(ctre.ControlMode.PercentOutput, 0)
 
     def autoIntake(self):
         if self.joystick.getRawButton(1):
@@ -115,6 +121,8 @@ class MyRobot(wpilib.TimedRobot):
         if self.joystick.getRawButton(2):
             # if not self.ballPresent(self.sensor3):
             self.step = -1
+            self.spark.set(0)
+            self.talon.set(ctre.ControlMode.PercentOutput, 0)
 
         if self.joystick.getRawButton(5):
             self.spark.set(-1)
@@ -126,14 +134,14 @@ class MyRobot(wpilib.TimedRobot):
         #     self.spark.set(0)
 
         self.speed = self.joystick.getRawAxis(3)
-        self.speed = 1 - (self.speed + 1)/2
+        self.speed = 1 - (self.speed + 1) / 2
 
-        #Steps
+        # Steps
         if self.step == 0:
-            self.talon.set(ctre.ControlMode.PercentOutput,-self.speed)
+            self.talon.set(ctre.ControlMode.PercentOutput, -self.speed)
             if self.ballPresent(self.sensor1):
                 self.step = 1
-                self.talon.set(ctre.ControlMode.PercentOutput,0)
+                self.talon.set(ctre.ControlMode.PercentOutput, 0)
 
         elif self.step == 1:
             self.spark.set(self.kBeltSpeed)
@@ -145,13 +153,13 @@ class MyRobot(wpilib.TimedRobot):
             if self.ballPresent(self.sensor2):
                 self.step = -1
                 self.spark.set(0)
-                self.talon.set(ctre.ControlMode.PercentOutput,0)
-
+                self.talon.set(ctre.ControlMode.PercentOutput, 0)
 
     def ballPresent(self, sensor):
         if sensor.getVoltage() > self.kBallTreshhold:
             return True
         return False
+
 
 if __name__ == "__main__":
 
